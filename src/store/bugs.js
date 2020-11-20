218fafd905aabd5d1e90ac756ff3e633db15fe36
ritabc <ritabc@gmail.com>
2020-11-19 20:51:53 -0500

    Use createAction and createReducer functions from reduxjs/toolkit to build store

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,55 +1,50 @@
-// Action Types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // Action creators
-export function bugAdded(description) {
-  return {
-    type: BUG_ADDED,
-    payload: {
-      // Use shorthand syntax for 'property: value' where property and value have the same name
-      description,
-    },
-  };
-}
+export const bugAdded = createAction("bugAdded");
+export const bugRemoved = createAction("bugRemoved");
+export const bugResolved = createAction("bugResolved");
 
-export const bugRemoved = (id) => ({
-  type: BUG_REMOVED,
-  payload: {
-    id: id,
+// Can write mutating code here: Redux toolkit will use immer to convert the code to non-mutating code behind the scenes
+export default createReducer([], {
+  // actions: functions
+  // bugAdded here must be the same as name in action Created
+  [bugAdded.type]: (bugs, action) => {
+    bugs.push({
+      id: ++lastID,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+  [bugResolved.type]: (bugs, action) => {
+    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+    bugs[index].resolved = true;
+  },
+  [bugRemoved.type]: (bugs, action) => {
+    bugs.filter((bug) => bug.id !== action.payload.id);
   },
 });
-
-export function bugResolved(id) {
-  return {
-    type: BUG_RESOLVED,
-    payload: {
-      id: id,
-    },
-  };
-}
 
 let lastID = 0;
 
 // Upon first run of reducer, state will be undefinied. In that case, we can set the default with our default '[]'
-export default function reducer(state = [], action) {
-  if (action.type === BUG_ADDED) {
-    return [
-      ...state,
-      {
-        id: ++lastID,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  } else if (action.type === BUG_REMOVED) {
-    return state.filter((bug) => bug.id !== action.payload.id);
-  } else if (action.type === BUG_RESOLVED) {
-    return state.map((bug) =>
-      // If bug.id is NOT what we received in the payload, return the bug as is. Otherwise, copy the bug and update it's resolved property
-      bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-    );
-  }
-  return state;
-}
+// export default function reducer(state = [], action) {
+//   if (action.type === bugAdded.type) {
+//     return [
+//       ...state,
+//       {
+//         id: ++lastID,
+//         description: action.payload.description,
+//         resolved: false,
+//       },
+//     ];
+//   } else if (action.type === bugRemoved.type) {
+//     return state.filter((bug) => bug.id !== action.payload.id);
+//   } else if (action.type === bugResolved.type) {
+//     return state.map((bug) =>
+//       // If bug.id is NOT what we received in the payload, return the bug as is. Otherwise, copy the bug and update it's resolved property
+//       bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
+//     );
+//   }
+//   return state;
+// }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,8 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import reducer from "./bugs";
 
-export default function configureStore() {
+export default function () {
   // pass reducer function to createStore()
-  const store = createStore(
-    reducer,
-    // Also allow use with Chrome Extension
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-  return store;
+  return configureStore({ reducer });
 }
