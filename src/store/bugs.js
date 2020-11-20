4762f19ba2f080e67ee896e33fb848dcaa3d09ee
ritabc <ritabc@gmail.com>
2020-11-19 22:07:35 -0500

    Use createSlice function from redux/toolkit to refactor bug store

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,50 +1,30 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-// Action creators
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
-
-// Can write mutating code here: Redux toolkit will use immer to convert the code to non-mutating code behind the scenes
-export default createReducer([], {
-  // actions: functions
-  // bugAdded here must be the same as name in action Created
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastID,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  },
-  [bugRemoved.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id !== action.payload.id);
-  },
-});
+import { createSlice } from "@reduxjs/toolkit";
 
 let lastID = 0;
 
-// Upon first run of reducer, state will be undefinied. In that case, we can set the default with our default '[]'
-// export default function reducer(state = [], action) {
-//   if (action.type === bugAdded.type) {
-//     return [
-//       ...state,
-//       {
-//         id: ++lastID,
-//         description: action.payload.description,
-//         resolved: false,
-//       },
-//     ];
-//   } else if (action.type === bugRemoved.type) {
-//     return state.filter((bug) => bug.id !== action.payload.id);
-//   } else if (action.type === bugResolved.type) {
-//     return state.map((bug) =>
-//       // If bug.id is NOT what we received in the payload, return the bug as is. Otherwise, copy the bug and update it's resolved property
-//       bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-//     );
-//   }
-//   return state;
-// }
+// calls redux/toolkit's createAction and createReducer (to enable us to write mutating code) functions:
+// Note the actions created here will be namespaced, ie: 'bugs/bugAdded'. For example, see the Redux tab in web dev tools
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  // maps action to action handlers
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastID,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+    bugRemoved: (bugs, action) => {
+      bugs.filter((bug) => bug.id !== action.payload.id);
+    },
+  },
+});
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
