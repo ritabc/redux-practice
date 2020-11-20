cf92bd06d9bc3c1ce891c23f43708b41edb375c6
ritabc <ritabc@gmail.com>
2020-11-20 11:40:57 -0500

    Add error / toast notification middleware

--- src/index.js
+++ src/index.js
@@ -47,3 +47,8 @@ store.dispatch((dispatch, getState) => {
   dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
   console.log(getState());
 });
+
+store.dispatch({
+  type: "error",
+  payload: { message: "An error occurred." },
+});
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,11 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import { logger, paramaterizedLogger } from "./middleware/logger";
 import func from "./middleware/func";
+import toast from "./middleware/toast";
 
 export default function () {
   // pass reducer function to createStore()
   return configureStore({
     reducer,
-    middleware: [logger, paramaterizedLogger("console"), func],
+    middleware: [toast, logger, paramaterizedLogger("console"), func],
   });
 }
