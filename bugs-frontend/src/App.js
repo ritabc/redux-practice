f3ebf004ec966fd2c5fde94378d752d89dc98c6c
ritabc <ritabc@gmail.com>
2020-11-21 18:39:18 -0500

    Use function component BugsList to connect store in new way

--- bugs-frontend/src/App.js
+++ bugs-frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Bugs from "./components/Bugs";
+import BugsList from "./components/Bugs";
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
 
@@ -8,7 +8,7 @@ const store = configureStore();
 function App() {
   return (
     <Provider store={store}>
-      <Bugs />;
+      <BugsList />;
     </Provider>
   );
 }
