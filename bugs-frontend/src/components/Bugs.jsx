b36cb0fb73f1b42ed44c41e7ec54dfc93c576d98
ritabc <ritabc@gmail.com>
2020-11-21 17:43:18 -0500

    Reorganize further and set up for react to import redux store

--- bugs-frontend/src/components/Bugs.jsx
+++ bugs-frontend/src/components/Bugs.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import StoreContext from "../contexts/storeContext";
 
 class Bugs extends Component {
   static contextType = StoreContext;
 
   componentDidMount() {
+    // subscribe so we get notified when the state of the store changes
+    // dispatch(loadBugs) to get list of bugs from backend
     console.log(this.context);
   }
   render() {
