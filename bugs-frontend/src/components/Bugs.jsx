ed565c9f578eaa6b5d5ae7d8509c7e7a8a370da2
ritabc <ritabc@gmail.com>
2020-11-21 17:53:15 -0500

    Subscribe to store and dispatch actions by hand (without react-redux)

--- bugs-frontend/src/components/Bugs.jsx
+++ bugs-frontend/src/components/Bugs.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import StoreContext from "../contexts/storeContext";
+import { loadBugs } from "../store/bugs";
 
 class Bugs extends Component {
   static contextType = StoreContext;
-
+  state = { bugs: [] };
   componentDidMount() {
+    const store = this.context;
+
     // subscribe so we get notified when the state of the store changes
+    this.unsubscribe = store.subscribe(() => {
+      const bugsInStore = store.getState().entities.bugs.list;
+      if (this.state.bugs !== bugsInStore) this.setState({ bugs: bugsInStore });
+    });
+
     // dispatch(loadBugs) to get list of bugs from backend
+    store.dispatch(loadBugs());
     console.log(this.context);
   }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
   render() {
-    return <div>Bugs</div>;
+    return (
+      <ul>
+        {this.state.bugs.map((bug) => (
+          <li key={bug.id}>{bug.description}</li>
+        ))}
+      </ul>
+    );
   }
 }
 
